c85b84e9b13971c6a5ffca0d32e71e9cbe8e4c21
joalorro <joalorro@gmail.com>
2018-11-14 21:53:08 -0500

    added swipeable

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
+import Swipeable from 'react-swipeable'
+import { throttle } from 'lodash'
+
 import Wrapper from './Wrapper'
 import CarouselContainer from './CarouselContainer'
 import CarouselSlot from './CarouselSlot'
+
+
 
 class Carousel extends Component {
 
@@ -30,9 +35,19 @@ class Carousel extends Component {
 		const { children } = this.props
 		const numItems = children.length || 1
 
-		this.setState({
-			position: position === numItems - 1 ? 0 : position + 1
-		}, () => this.doSliding(this.state.position));
+		this.doSliding('next', position === numItems - 1 ? 0 : position + 1)
+		
+		// this.setState({
+		// 	position: position === numItems - 1 ? 0 : position + 1
+		// }, () => this.doSliding(this.state.position));
+	}
+
+	prevSlide = () => {
+		const { position } = this.state
+		const { children } = this.props
+		const numItems = children.length 
+
+		this.doSliding('prev', position === 0 ? numItems - 1 : position - 1)
 	}
 
 	doSliding = (direction,position) => {
@@ -45,22 +60,31 @@ class Carousel extends Component {
 		setTimeout( () => this.setState({ sliding: false }), 50)
 	}
 
+	handleSwipe = throttle((isNext) => {
+		if (!isNext) this.nextSlide()
+		else this.prevSlide()
+	}, 500, { trailing: false })
+
 	render() {
 
 		const { title, children } = this.props
+		const { direction, sliding } = this.state
 
 		return (
 			<div>
 				<h2>{ title }</h2>
-				<Wrapper>
-					<CarouselContainer sliding={this.state.sliding}>
-						{ children.map( (child,index) => {
-							return <CarouselSlot key={index} order={ this.getOrder(index) }>
-								{child} 
-							</CarouselSlot>
-						}) }
-					</CarouselContainer>
-				</Wrapper>
+				<Swipeable onSwipingLeft={ () => this.handleSwipe(true)} onSwipingRight={ () => this.handleSwipe()} >
+					<Wrapper>
+						<CarouselContainer direction={direction} sliding={sliding}>
+							{ children.map( (child,index) => {
+								return <CarouselSlot key={index} order={ this.getOrder(index) }>
+									{child} 
+								</CarouselSlot>
+							}) }
+						</CarouselContainer>
+					</Wrapper>
+				</Swipeable>
+				<button onClick={this.prevSlide}>Prev</button>
 				<button onClick={this.nextSlide}>Next</button>
 			</div>
 		);
--- src/components/CarouselContainer.js
+++ src/components/CarouselContainer.js
@@ -3,9 +3,11 @@ import styled from 'styled-components'
 const CarouselContainer = styled.div`
 	display: flex;
 	margin: 0 0 20px 20px;
-	transition: ${({sliding}) => sliding ? 'none' : 'transform 1s ease'};
-	transform: ${({sliding}) => {
+	transition: ${({ sliding }) => sliding ? 'none' : 'transform 1s ease'};
+	transform: ${({ sliding, direction }) => {
 		if (!sliding) return 'translateX(calc(-80% - 20px))'
+		if (direction === 'prev') return 'translateX(calc(2 * (-80% - 20px)))'
+		
 		return 'translateX(0%)'
 	}};
 `
